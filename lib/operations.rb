16f949c68340125be9cf63a49567f0b7a44c2ef1
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-09 01:45:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,8 +8,9 @@ end
 
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? "true" : "false"
-	speed > 40 || speed < 60 ? "false" : "true"
+	#speed < 40 || speed > 60 ? "true" : "false"
+	#speed > 40 || speed < 60 ? "false" : "true"
+	speed ? speed 
 end
 	
 
